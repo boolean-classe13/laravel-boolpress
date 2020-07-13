949ecb862c8bb42cb8db5c6103d6b7f43512e64f
Sofia <Perlari>
2020-07-13 13:13:01 +0200

    CRUD lato admin per i post protetta da autenticazione + predisposti layout pubblico e admin

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <h1>Benvenuto nel nostro blog</h1>
+        </div>
+    </div>
+</div>
+@endsection
