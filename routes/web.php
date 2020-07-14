5548011edc549c7cd2b33f3c7b3ba84f2e668fb2
Sofia <Perlari>
2020-07-14 11:17:04 +0200

    visualizzazione post con interfaccia pubblica

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Auth::routes();
+Auth::routes(['register' => false]);
+// Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
+Route::get('/posts', 'PostController@index')->name('posts.index');
+Route::get('/posts/{slug}', 'PostController@show')->name('posts.show');
 
 Route::prefix('/admin')->namespace('Admin')->name('admin.')->middleware('auth')->group(function () {
     Route::get('/', 'HomeController@index')->name('home');
